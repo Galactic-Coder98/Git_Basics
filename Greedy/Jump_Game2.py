c60fbb53adf8d74c8dd88f531cde241e256f7bcf
Galactic-Coder98 <princechowdury32@gmail.com>
2024-11-01 11:51:53 -0700

    Adding Greedy folder and Jump Game 2 Problem Solution

---
+++ Greedy/Jump_Game2.py
@@ -0,0 +1,16 @@
+def jump_game_two(nums):
+  farthest_jump = 0
+  current_jump = 0
+  jumps = 0
+
+  for i in range(len(nums) - 1):
+    farthest_jump = max(farthest_jump, i + nums[i])
+
+    if i == current_jump:
+      jumps += 1
+      current_jump = farthest_jump
+
+      if current_jump >= len(nums) - 1:
+        break
+
+  return jumps
